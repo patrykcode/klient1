8da21a6859aa32df402787642dbd64f16b61b898
patrykP <patryk@redicon.pl>
2019-05-29 21:27:05 +0200

    csv

--- modules/personsCms/src/Http/Controllers/PersonsController.php
+++ modules/personsCms/src/Http/Controllers/PersonsController.php
@@ -58,11 +58,9 @@ class PersonsController extends AdminController {
 
         fclose($fp);
 
+        header('Content-Encoding: UTF-8');
+        header('Content-type: text/csv; charset=UTF-8');
         header('Content-Disposition: attachment; filename="'.$filename.'"');
-        header("Content-Type:   application/vnd.ms-excel; charset=utf-8");
-        header("Expires: 0");
-        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
-        header("Cache-Control: private", false);
 
         return \Storage::get($filename);
     }
